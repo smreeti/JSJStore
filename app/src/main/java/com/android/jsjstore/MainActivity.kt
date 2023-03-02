a4163cfe0ed3c111ae9f88f71447897a3c395111
smriti.mool <mool.smreeti@gmail.com>
2023-03-02 09:31:37 -0500

    add NoActionBar in themes.xml

--- app/src/main/java/com/android/jsjstore/MainActivity.kt
+++ app/src/main/java/com/android/jsjstore/MainActivity.kt
@@ -21,7 +21,6 @@ import com.google.firebase.auth.ktx.auth
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.ktx.Firebase
-import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
